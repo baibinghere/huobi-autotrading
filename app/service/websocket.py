eee7bc08c1a216123261778cde1fd9eaff6d74e2
baib2 <bing.bai@emc.com>
2018-08-06 10:59:24 +0800

    Huobipro的wss URL似乎改了，修改为官网上查询到的地址

--- app/service/websocket.py
+++ app/service/websocket.py
@@ -74,7 +74,7 @@ def on_open(ws):
 
 def start():
     ws = websocket.WebSocketApp(
-        "wss://api.huobipro.com/ws",
+        "wss://www.huobi.com/-/s/pro/ws",
         on_open=on_open,
         on_message=on_message,
         on_error=on_error,
